ef37a05da1b3b601accaee85b96597c8053a2678
Jianyou Dai <jianyou.dai@elmeragroup.no>
2025-12-19 19:59:32 +0100

    add  sanity plugin media

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 import deskStructure from './deskStructure'
 import {TeaPotIcon} from './icons/TeaIcons'
@@ -13,7 +14,7 @@ export default defineConfig({
   projectId: process.env.SANITY_PROJECT_ID || '3hojl0zw',
   dataset: process.env.SANITY_DATASET || 'production',
 
-  plugins: [structureTool({structure: deskStructure}), visionTool()],
+  plugins: [structureTool({structure: deskStructure}), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
